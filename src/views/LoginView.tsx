2747f69d0c52e59e004133eb8cff56a8b90a4408
Donato De Battista <doni.db@hotmail.com>
2025-06-14 15:37:08 -0300

    router Links

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom"
+
 export default function LoginView() {
   return (
-    <div className="text-3xl">LoginView</div>
+    <>
+        <div className="text-3xl">LoginView</div>
+
+        <nav>
+            <Link to="/auth/register" className="text-blue-500 hover:underline">
+                ¿No tienes cuenta? Regístrate
+            </Link>
+        </nav>
+    </>
   )
 }
--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom"
+
 export default function RegisterView() {
   return (
-    <div className="text-3xl">RegisterView</div>
+    <>
+        <div className="text-3xl">RegisterView</div>
+        <nav>
+            <Link to={"/auth/login"} className="text-blue-500 hover:underline">
+                ¿Ya tienes cuenta? Inicia sesión
+            </Link>
+        </nav>
+    </>
   )
 }
