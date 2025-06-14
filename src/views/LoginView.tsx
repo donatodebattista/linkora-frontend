39c9d60551f6a67eba4e74502a132d8def5a4ace
Donato De Battista <doni.db@hotmail.com>
2025-06-14 11:55:32 -0300

    router initial config

---
+++ src/views/LoginView.tsx
@@ -0,0 +1,5 @@
+export default function LoginView() {
+  return (
+    <div className="text-3xl">LoginView</div>
+  )
+}
---
+++ src/views/RegisterView.tsx
@@ -0,0 +1,5 @@
+export default function RegisterView() {
+  return (
+    <div className="text-3xl">RegisterView</div>
+  )
+}
