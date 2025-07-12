9c10afac34eab345c9830d36a345c3c3c2111d3b
donatodebattista <doni.db@hotmail.com>
2025-07-12 11:45:56 -0300

    get users data from server

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -5,4 +5,14 @@ const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 })
 
+
+//Interceptor para agregar el token de autenticación a las peticiones
+axios.interceptors.request.use((config) => {
+    const token = localStorage.getItem("AUTH_TOKEN");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+})
+
 export default api
